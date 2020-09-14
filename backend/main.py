b39eb26b64f732c9cf84aec2c6791db27da4daca
Senarii <vortekdynamic@gmail.com>
2020-09-14 19:10:17 -0400

    Dynamic reloading of FastAPI backend

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World123"}
+
+
+@app.get("/images")
+async def images():
+    results = {
+        'imagesTotal': 0,
+        'imagesProcessed': 0,
+        'imagesProcessing': 0,
+        'imagesErrored': 0,
+        'images': {'image1': 256, 'image2': 512}
+    }
+
+    return results
