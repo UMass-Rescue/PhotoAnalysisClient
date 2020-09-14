0f48c35186a347eb1a2990d581c66c520ff3dfd8
Senarii <vortekdynamic@gmail.com>
2020-09-13 20:07:51 -0400

    Fix docker not auto-reloading on changes in React

--- frontend/src/views/Import/Import.js
+++ frontend/src/views/Import/Import.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/styles';
-import { Grid } from '@material-ui/core';
-
+import {makeStyles} from '@material-ui/styles';
+import {Grid} from '@material-ui/core';
 
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    padding: theme.spacing(4)
-  }
+    root: {
+        padding: theme.spacing(4)
+    }
 }));
 
 const Import = () => {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <Grid
-        container
-        spacing={4}
-      >
-      </Grid>
-    </div>
-  );
+    return (
+        <div className={classes.root}>
+            <Grid container spacing={4}>
+                <Grid item>
+                   abc
+                </Grid>
+
+            </Grid>
+        </div>
+    );
 };
 
 export default Import;
